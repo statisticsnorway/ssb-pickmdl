6035ccdf9bf8ad6a37c641c9c5a895b9811ca38c
Langsrud <oyl@ssb.no>
2022-03-10 16:14:40 +0100

    star parameters Index to be selected and function to be called  when no model is ok according to criteria.

--- R/crit_selection.R
+++ R/crit_selection.R
@@ -6,7 +6,9 @@
 #' @param pickmdl_method A replacement for the original `PICKMDL` argument, `method`. 
 #'          Possible values are `"first"`(default) and `"aic"`. 
 #'          The latter is an alternative to the original method, `"best"` (not implemented). 
-#'
+#' @param star Index to be selected when no model is ok according to criteria. 
+#' @param when_star Function to be called when no model is ok according to criteria. 
+#'                  Supply `stop` to invoke error.  Supply `NULL` to do nothing.
 #' @return Selected index
 #' @export
 #'
@@ -22,10 +24,14 @@
 #' crit_selection(tab)
 #' crit_selection(tab[2:5, ])
 #' crit_selection(tab[1:2, ]) # Warning
+#' crit_selection(tab[1:2, ], star = 2, when_star = message) 
 #' crit_selection(tab[5:1, ])
 #' crit_selection(tab[5:1, ], pickmdl_method = "aic")
 #' 
-crit_selection <- function(crit_tab, pickmdl_method = "first"){
+crit_selection <- function(crit_tab, pickmdl_method = "first", star = 1, when_star = warning){
+  if (star > nrow(crit_tab)) {
+    stop("The value of star is too large")
+  }
   if(!(pickmdl_method %in% c("first", "aic")))
     stop('Allowed values of parameter pickmdl_method are "first" and "aic")')
   
@@ -34,10 +40,12 @@ crit_selection <- function(crit_tab, pickmdl_method = "first"){
   } else {
     x <- rep(1, nrow(crit_tab))
   }
-  
   x[!(crit_tab[,"crit1"] < 0.15 & crit_tab[,"crit2"] > 0.05 & crit_tab[,"crit3"] < 0.9)] <- Inf
-  if (min(x) == Inf){
-    warning("No model is ok according to criteria")
+  if (min(x) == Inf) {
+    if (!is.null(when_star)) {
+      when_star("No model is ok according to criteria")
+    }
+    return(star)
   }
   which.min(x)
 }
--- R/x13_pickmdl.R
+++ R/x13_pickmdl.R
@@ -7,6 +7,8 @@
 #' @param spec List of several `x13_spec` output objects. That is, `spec` can be output from  \code{\link{x13_spec_pickmdl}}.
 #' @param ... Further `x13` parameters (currently only parameter `userdefined` is additional parameter to `x13`).
 #' @param pickmdl_method \code{\link{crit_selection}} parameter
+#' @param star           \code{\link{crit_selection}} parameter
+#' @param when_star      \code{\link{crit_selection}} parameter
 #'
 #' @return An `x13` output object
 #' @export
@@ -41,9 +43,15 @@
 #' 
 #' # Warning when transform.function = "None"
 #' spec_d  <- x13_spec_pickmdl(spec = "RSA3", transform.function = "None")
-#' d <- x13_pickmdl(myseries, spec_d) 
+#' d <- x13_pickmdl(myseries, spec_d)
+#' d$regarima 
+#' 
+#' # Warning avoided (when_star) and 2nd (star) model selected 
+#' e <- x13_pickmdl(myseries, spec_d, star = 2, when_star = NULL)
+#' e$regarima   
 #'                                           
-x13_pickmdl <- function(series, spec, ..., pickmdl_method = "first") {
+x13_pickmdl <- function(series, spec, ..., 
+                        pickmdl_method = "first", star = 1, when_star = warning) {
   
   if (!all(apply(sapply(spec, class),1, unique) == c("SA_spec", "X13"))) {
     stop("`spec` must be a list of `x13_spec` output objects. Run `x13_spec_pickmdl`?")
@@ -53,7 +61,8 @@ x13_pickmdl <- function(series, spec, ..., pickmdl_method = "first") {
   
   crit_tab <- crit_table(sa_mult)
   
-  mdl_nr <- crit_selection(crit_tab, pickmdl_method = pickmdl_method)
+  mdl_nr <- crit_selection(crit_tab, pickmdl_method = pickmdl_method, 
+                           star = star, when_star = when_star)
   
   # easy to return sa_mult[[mdl_nr]], but prepare for more general code
   # series for selection and final series for x13 may be different 
